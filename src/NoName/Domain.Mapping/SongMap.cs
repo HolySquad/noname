3618a3db4e3738da24ce1a4c6c94c4be5c8ef589
iliusha10 <ilia.dermenji@yahoo.com>
2015-10-27 12:33:29 +0200

    Added missing files

---
+++ src/NoName/Domain.Mapping/SongMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Audio;
+using FluentNHibernate.Mapping;
+using NHibernate.Mapping;
+
+namespace Domain.Mapping
+{
+    public class SongMap :SubclassMap<Song>
+    {
+        public SongMap()
+        {
+
+        }
+
+    }
+}
--- src/NoName/NoName/Audio/Song.cs
+++ src/NoName/NoName/Audio/Song.cs
@@ -5,15 +5,21 @@ namespace Domain.Audio
     public class Song : AudioFile
     {
         //TODO need to think about methods Play/Stop/etc.
-        public Artist Artist { get; protected set; }
-        public Album Album { get; protected set; }
-        public TimeSpan Duration { get; protected set; }
+
+        public virtual Artist Artist { get; protected set; }
+        public virtual Album Album { get; protected set; }
+        public virtual TimeSpan Duration { get; protected set; }
 
         public Song(Artist artist, Album album, TimeSpan duration)
         {
             Artist = artist;
             Album = album;
             Duration = duration;
+        }
+
+        [Obsolete]
+        protected Song()
+        {
         }
     }
 }
