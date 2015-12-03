6ea03bfece8fbc8160c3c61251afee5d877371b0
iliusha10 <ilia.dermenji@yahoo.com>
2015-12-03 09:36:11 +0200

    Added absent files

---
+++ src/NoName/NoName/FileType.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace Domain
+{
+    public enum FileType
+    {
+        Undefined = 0,
+        [Description(".mp3")]
+        Mp3 = 1,
+        [Description(".wma")]
+        Wma = 2,
+        [Description(".flac")]
+        Flac = 3,
+        [Description(".mp4")]
+        Mp4 = 4,
+        [Description(".mov")]
+        Mov = 5,
+        [Description(".mpeg")]
+        Mpeg = 6,
+    }
+
+
+}
---
+++ src/NoName/NoName/Playlist.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Audio;
+
+namespace Domain
+{
+    public class Playlist: Entity
+    {
+        [Obsolete]
+        protected Playlist()
+        {
+        }
+
+        public Playlist(string playlistName)
+        {
+            PlaylistName = playlistName;
+            PlaylistLenght = TimeSpan.Zero;
+            SongCount = 0;
+        }
+        public virtual string PlaylistName { get; protected set; }
+        public virtual TimeSpan PlaylistLenght { get; protected set; }
+        public virtual int SongCount { get; protected set; }
+
+        public virtual IList<PlaylistSongs> PlaylistSongs { get; protected set; }
+
+        //public virtual void UpdateLenght()
+        //{
+        //    foreach (var song in ListOfSongs)
+        //    {
+        //        PlaylistLenght = PlaylistLenght + song.Duration;
+        //    }
+            
+        //}
+        
+        //public virtual void AddSong(Song song)
+        //{
+        //    ListOfSongs.Add(song);
+        //    SongCount++;
+        //}
+
+        //public virtual void RemoveSong(Song song)
+        //{
+        //    ListOfSongs.Remove(song);
+        //    SongCount--;
+        //}
+
+    }
+}
