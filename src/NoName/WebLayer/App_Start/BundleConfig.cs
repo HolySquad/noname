e09eb8567fc5cc4d98096b5f2ea5785e0a7f33e9
Adamster <adamsterdomi@gmail.com>
2016-01-04 11:36:47 +0200

    navbar in _layout

--- src/NoName/WebLayer/App_Start/BundleConfig.cs
+++ src/NoName/WebLayer/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace WebLayer
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-1.9.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
