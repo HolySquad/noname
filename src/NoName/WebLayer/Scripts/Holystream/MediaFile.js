175d227cb96766684ed51ecf028756fa8de5ce49
Adamster Domi <adamsterdomi@gmail.com>
2016-01-26 21:14:34 +0200

    jquery update

---
+++ src/NoName/WebLayer/Scripts/Holystream/MediaFile.js
@@ -0,0 +1,19 @@
+﻿function MediaFileCall() {
+    var self = this;
+
+    var internal = {
+        addFileForm: "#addFileForm",
+        fileUploadDialogId: "filesUploadForm"
+    };
+    self.addFileUrl = "";
+    self.detailsFileUrl = "";
+    self.deleteFilesUrl = "";
+
+    self.uploadFile = function () {
+        $(internal.fileUploadDialogId).load(self.addFileUrl).dialog('open');
+    };
+   
+    internal.upload = function() {
+
+    };
+};
